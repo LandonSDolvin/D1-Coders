39deceb16145c7744bcba8aac6f9d7b9d57e8dee
Spencer Lin <spencerlin@dhcp-10-29-69-154.dyn.MIT.EDU>
2023-01-16 23:14:23 -0500

    created joined groups component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import JoinedGroups from "./components/pages/JoinedGroups";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact />
-        </Routes>
+        <Navbar/>
+        <JoinedGroups path="/joinedgroups" component={JoinedGroups} />
       </Router>
     </>
   );
---
+++ src/components/pages/JoinedGroups.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function JoinedGroups(){
+    return(
+        <div> Joined! </div>
+    )
+}
+
+export default JoinedGroups
